d85f9c719d5791ab0e0731a2594f5f204354bdf4
Lizzy <martinezdelgadoelisabeth@gmail.com>
2025-08-01 21:31:38 +0200

    added a few details to the dialogue (sound and waiting animation)

--- base/ui.gd
+++ base/ui.gd
@@ -7,8 +7,8 @@ var character_name: String
 
 
 func _on_play_pressed() -> void:
-	$Menus/BaseMenu/Main/Buttons.visible = false
-	$Menus/BaseMenu/Main/Username.visible = true
+	$Menus/BaseMenu/Main/Buttons.hide()
+	$Menus/BaseMenu/Main/Username.show()
 
 func _on_settings_pressed() -> void:
 	pass # Replace with function body.
@@ -20,32 +20,32 @@ func _on_quit_pressed() -> void:
 
 func _on_confirm_pressed() -> void:
 	if not $Menus/BaseMenu/Main/Username/Username.text:
-		$Menus/BaseMenu/Main/Username/EmptyWarning.visible = true
+		$Menus/BaseMenu/Main/Username/EmptyWarning.show()
 	else:
 		character_name = $Menus/BaseMenu/Main/Username/Username.text
 		emit_signal("character_selected", character_name)
-		$Menus/BaseMenu/Main.visible = false
-		$Menus/BaseMenu/InGame.visible = true
-		$Menus/BaseMenu/Main/Username.visible = false
-		$Menus/BaseMenu/Main/Username/LoadWarning.visible = false
-		$Menus/BaseMenu/Main/Username/EmptyWarning.visible = false
-		$Menus/BaseMenu/Main/Buttons.visible = true
+		$Menus/BaseMenu/Main.hide()
+		$Menus/BaseMenu/InGame.show()
+		$Menus/BaseMenu/Main/Username.hide()
+		$Menus/BaseMenu/Main/Username/LoadWarning.hide()
+		$Menus/BaseMenu/Main/Username/EmptyWarning.hide()
+		$Menus/BaseMenu/Main/Buttons.show()
 
 
 func _on_pause_pressed() -> void:
-	$Menus/BaseMenu/InGame.visible = false
-	$Menus/BaseMenu/Pause.visible = true
+	$Menus/BaseMenu/InGame.hide()
+	$Menus/BaseMenu/Pause.show()
 
 
 func _on_resume_pressed() -> void:
-	$Menus/BaseMenu/Pause.visible = false
-	$Menus/BaseMenu/InGame.visible = true
+	$Menus/BaseMenu/Pause.hide()
+	$Menus/BaseMenu/InGame.show()
 
 
 func _on_main_menu_pressed() -> void:
-	$Menus/BaseMenu/Pause.visible = false
-	$Menus/BaseMenu/Main.visible = true
+	$Menus/BaseMenu/Pause.hide()
+	$Menus/BaseMenu/Main.show()
 
 
-func _on_username_text_submitted(new_text: String) -> void:
+func _on_username_text_submitted(_new_text: String) -> void:
 	_on_confirm_pressed()
